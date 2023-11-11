f3ad81a5e0fd8aa9c7b8f8e27f8aae9cade3116f
Travis Fischer <fisch0920@gmail.com>
2023-11-11 03:58:02 -0600

    📠

--- src/threads.ts
+++ src/threads.ts
@@ -1,6 +1,9 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
+import pMap from 'p-map'
 
 import * as routes from './generated/oai-routes'
+import * as utils from './utils'
+import { prisma } from './db'
 
 const app: OpenAPIHono = new OpenAPIHono()
 
@@ -8,18 +11,59 @@ app.openapi(routes.createThread, async (c) => {
   const body = c.req.valid('json')
   console.log('createThread', { body })
 
-  // TODO
+  const { messages, ...data } = utils.convertOAIToPrisma(body)
 
-  return c.jsonT({} as any)
+  const thread = await prisma.thread.create({
+    data
+  })
+
+  await pMap(
+    messages,
+    async (message) => {
+      const { content, ...data } = utils.convertOAIToPrisma(message)
+
+      if (data.file_ids && data.file_ids.length > 10) {
+        c.status(400)
+        c.text('Too many files')
+        throw new Error('Too many files')
+      }
+
+      await prisma.message.create({
+        data: {
+          ...data,
+          content: [
+            {
+              type: 'text',
+              text: {
+                value: content,
+                annotations: []
+              }
+            }
+          ],
+          thread_id: thread.id
+        }
+      })
+    },
+    {
+      concurrency: 8
+    }
+  )
+
+  return c.jsonT(utils.convertPrismaToOAI(thread))
 })
 
 app.openapi(routes.getThread, async (c) => {
   const { thread_id } = c.req.valid('param')
   console.log('getThread', { thread_id })
 
-  // TODO
+  const res = await prisma.thread.findUnique({
+    where: {
+      id: thread_id
+    }
+  })
 
-  return c.jsonT({} as any)
+  if (!res) return c.notFound() as any
+  return c.jsonT(utils.convertPrismaToOAI(res))
 })
 
 app.openapi(routes.modifyThread, async (c) => {
@@ -27,20 +71,29 @@ app.openapi(routes.modifyThread, async (c) => {
   const body = c.req.valid('json')
   console.log('modifyThread', { thread_id, body })
 
-  // TODO
+  const res = await prisma.message.update({
+    where: {
+      id: thread_id
+    },
+    data: utils.convertOAIToPrisma(body)
+  })
 
-  return c.jsonT({} as any)
+  return c.jsonT(utils.convertPrismaToOAI(res))
 })
 
 app.openapi(routes.deleteThread, async (c) => {
   const { thread_id } = c.req.valid('param')
   console.log('deleteThread', { thread_id })
 
-  // TODO
+  const res = await prisma.assistantFile.delete({
+    where: {
+      id: thread_id
+    }
+  })
 
   return c.jsonT({
     deleted: true,
-    id: thread_id,
+    id: res.id,
     object: 'thread.deleted' as const
   })
 })
